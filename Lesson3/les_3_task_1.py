75787c4c7752bab723244695012fee1f5c5f85c0
noteasycode <58310014+noteasycode@users.noreply.github.com>
2020-04-03 01:34:58 +0300

    New commit

--- Lesson3/les_3_task_1.py
+++ Lesson3/les_3_task_1.py
@@ -3,12 +3,18 @@
 сколько из них кратны каждому из чисел в диапазоне от 2 до 9.
 Примечание: 8 разных ответов.
 """
-import random
 
 a = [_ for _ in range(2, 100)]
 b = [_ for _ in range(2, 10)]
-print(a)
-print()
-for value in a, b:
-    if a[value] % b[value] == 0:
-        print(value)
+result = [0]*8
+
+for i, j in a:
+    for j in b:
+        if i % j == 0:
+            result[j-2] += 1
+print(result)
+
+i = 0
+while i < len(result):
+    print(f"{i+2} | {result[i]}")
+    i += 1
